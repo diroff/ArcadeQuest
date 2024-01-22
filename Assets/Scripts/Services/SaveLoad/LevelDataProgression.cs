6da7441cbf67c2c9e20ba3b1db02e8ab373a6e28
Диэр <danil.aprelskiy@mail.ru>
2024-01-22 12:54:39 +0300

    feat: add save-load system
    Добавлена базовая система для сохранения/загрузки;
    Добавлено сохранение данных о последнем уровне.

---
+++ Assets/Scripts/Services/SaveLoad/LevelDataProgression.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class LevelDataProgression : MonoBehaviour
+{
+    public const string Key = "LevelProgression";
+
+    private IStorageService _storageService;
+    private LevelProgression _levelData;
+
+    private void Awake()
+    {
+        _storageService = new JsonToFileStorageService();
+    }
+
+    [ContextMenu("Save current level")]
+    public void Save(string sceneName)
+    {
+        LevelProgression data = new LevelProgression();
+
+        data.LevelName = sceneName;
+
+        _storageService.Save(Key, data);
+    }
+
+    public void Load()
+    {
+        _storageService.Load<LevelProgression>(Key, data =>
+        {
+            _levelData = data;
+        });
+    }
+
+    public LevelProgression GetData()
+    {
+        Load();
+        return _levelData;
+    }
+
+    public void SetDefaultSettings()
+    {
+        Save("MainLevel_1");
+    }
+}
+
+public class LevelProgression
+{
+    public string LevelName;
+}
