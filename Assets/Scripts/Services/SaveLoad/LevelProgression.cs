82ae8eb751c5dab3a8eb0dee8d3809d6192e9397
Диэр <danil.aprelskiy@mail.ru>
2024-01-27 13:11:50 +0300

    feat: add player/camera position in save progress
    При сохранении прогресса, добавляются данные о позиции/повороте игрока/камеры;
    При загрузке сейва устанавливаются соответствующие данные.

--- Assets/Scripts/Services/SaveLoad/LevelProgression.cs
+++ Assets/Scripts/Services/SaveLoad/LevelProgression.cs
@@ -9,6 +9,9 @@ public class LevelProgression : MonoBehaviour
     [SerializeField] private LevelItems _levelItems;
     [SerializeField] private LevelBonuses _levelBonuses;
 
+    [SerializeField] private Player _player;
+    [SerializeField] private Camera _camera;
+
     private IStorageService _storageService;
     private LevelProgress _levelData;
 
@@ -21,7 +24,7 @@ public class LevelProgression : MonoBehaviour
             if (data == default)
                 FirstLevelSave(SceneManager.GetActiveScene().name);
 
-            _levelData = data;
+            Load();
         });
     }
 
@@ -49,6 +52,12 @@ public class LevelProgression : MonoBehaviour
             return;
 
         DestroyCollectedItems();
+
+        if (_levelData.PlayerPosition != null)
+            SetPlayerPositions();
+
+        if (_levelData.CameraPosition != null)
+            SetCameraPosition();
     }
 
     private void DestroyCollectedItems()
@@ -60,20 +69,77 @@ public class LevelProgression : MonoBehaviour
             GameObject.Find(item).GetComponent<Bonus>().Collect();
     }
 
+    private void SetPlayerPositions()
+    {
+        var position = _levelData.PlayerPosition;
+        var rotation = _levelData.PlayerRotation;
+
+        _player.transform.position = new Vector3(position.x, position.y, position.z);
+        _player.transform.localRotation = new Quaternion(rotation.x, rotation.y, rotation.z, rotation.w);
+    }
+
+    private void SetCameraPosition()
+    {
+        var position = _levelData.CameraPosition;
+        var rotation = _levelData.CameraRotation;
+
+        _camera.transform.position = new Vector3(position.x, position.y, position.z);
+        _camera.transform.localRotation = new Quaternion(rotation.x, rotation.y, rotation.z, rotation.w);
+    }
+
     public void SaveCollectedItem(string itemName)
     {
-        if (!_levelData.CollectedItems.Contains(itemName))
-            _levelData.CollectedItems.Add(itemName);
+        if (_levelData.CollectedItems.Contains(itemName))
+            return;
+
+        _levelData.CollectedItems.Add(itemName);
+
+        SavePlayerPlacement();
+        SaveCameraPlacement();
 
         _storageService.Save(Key, _levelData);
     }
 
     public void SaveCollectedBonus(string bonusName)
     {
-        if(!_levelData.CollectedBonus.Contains(bonusName))
-            _levelData.CollectedBonus.Add(bonusName);
+        if (_levelData.CollectedBonus.Contains(bonusName))
+            return;
+
+        _levelData.CollectedBonus.Add(bonusName);
+
+        SavePlayerPlacement();
+        SaveCameraPlacement();
 
         _storageService.Save(Key, _levelData);
+    }
+
+    private void SavePlayerPlacement()
+    {
+        _levelData.PlayerPosition = new CurrentPosition();
+        _levelData.PlayerRotation = new CurrentRotation();
+
+        _levelData.PlayerPosition.x = _player.gameObject.transform.position.x;
+        _levelData.PlayerPosition.y = _player.gameObject.transform.position.y;
+        _levelData.PlayerPosition.z = _player.gameObject.transform.position.z;
+
+        _levelData.PlayerRotation.x = _player.gameObject.transform.rotation.x;
+        _levelData.PlayerRotation.y = _player.gameObject.transform.rotation.y;
+        _levelData.PlayerRotation.z = _player.gameObject.transform.rotation.z;
+        _levelData.PlayerRotation.w = _player.gameObject.transform.rotation.w;
+    }
+
+    private void SaveCameraPlacement()
+    {
+        _levelData.CameraPosition = new CurrentPosition();
+
+        _levelData.CameraPosition.x = _camera.gameObject.transform.position.x;
+        _levelData.CameraPosition.y = _camera.gameObject.transform.position.y;
+        _levelData.CameraPosition.z = _camera.gameObject.transform.position.z;
+
+        _levelData.CameraRotation.x = _camera.gameObject.transform.rotation.x;
+        _levelData.CameraRotation.y = _camera.gameObject.transform.rotation.y;
+        _levelData.CameraRotation.z = _camera.gameObject.transform.rotation.z;
+        _levelData.CameraRotation.w = _camera.gameObject.transform.rotation.w;
     }
 
     public void Load()
@@ -108,4 +174,25 @@ public class LevelProgress
     public CurrentLevel Level;
     public List<string> CollectedItems = new List<string>();
     public List<string> CollectedBonus = new List<string>();
+    
+    public CurrentPosition PlayerPosition;
+    public CurrentRotation PlayerRotation;
+
+    public CurrentPosition CameraPosition;
+    public CurrentRotation CameraRotation;
+}
+
+public class CurrentPosition
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+public struct CurrentRotation
+{
+    public float x;
+    public float y;
+    public float z;
+    public float w;
 }
