7a3bb2c097cfbe953c28d74355db834726454780
Диэр <danil.aprelskiy@mail.ru>
2024-05-21 16:25:51 +0300

    feat: add more animations with correct settings

--- Assets/Scripts/Creature/Player/CreatureAnimator.cs
+++ Assets/Scripts/Creature/Player/CreatureAnimator.cs
@@ -7,10 +7,20 @@ public class CreatureAnimator : MonoBehaviour
     private int _horizontal;
     private int _vertical;
 
+    private int _wait;
+    private int _phone;
+    private int _happy;
+    private int _walk;
+
     private void Awake()
     {
         _horizontal = Animator.StringToHash("Horizontal");
         _vertical = Animator.StringToHash("Vertical");
+        _wait = Animator.StringToHash("Wait");
+        _walk = Animator.StringToHash("Walk");
+        _phone = Animator.StringToHash("Phone");
+        _happy = Animator.StringToHash("Happy");
+
     }
 
     public void UpdateAnimatorValues(float horizontalMovement, float verticalMovement)
@@ -50,5 +60,25 @@ public class CreatureAnimator : MonoBehaviour
 
         _animator.SetFloat(_horizontal, snappedHorizontal, 0.1f, Time.deltaTime);
         _animator.SetFloat(_vertical, snappedVertical, 0.1f, Time.deltaTime);
+    }
+
+    public void SetWaitPose()
+    {
+        _animator.SetTrigger(_wait);
+    }
+
+    public void SetPhonePose()
+    {
+        _animator.SetTrigger(_phone);
+    }
+
+    public void SetHappyPose()
+    {
+        _animator.SetTrigger(_happy);
+    }
+
+    public void ContinueWalk()
+    {
+        _animator.SetTrigger(_walk);
     }
 }
