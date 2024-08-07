e7eef40b7b1715be64db76d98a2425054e3b034c
Диэр <danil.aprelskiy@mail.ru>
2024-08-07 13:17:01 +0300

    feat: add clear slots if meta panel is full

--- Assets/Scripts/Meta/MetaSlotPanel.cs
+++ Assets/Scripts/Meta/MetaSlotPanel.cs
@@ -27,12 +27,26 @@ public class MetaSlotPanel : MonoBehaviour
         int currentPairCount = pairs.Count;
 
         if (currentPairCount < _countToPaired)
+        {
+            if (IsFull())
+                StartCoroutine(ClearSlotsAfterAnimation());
+
             return;
+        }
 
         foreach (var pair in pairs)
             pair.DisablePhysic();
 
         StartCoroutine(CollectItems(pairs));
+    }
+
+    private IEnumerator ClearSlotsAfterAnimation()
+    {
+        var items = GetItems();
+
+        yield return new WaitUntil(() => items.All(item => item.Animation.IsCollectAnimationFinished));
+
+        ClearSlots();
     }
 
     private IEnumerator CollectItems(List<MetaItem> items)
@@ -59,6 +73,18 @@ public class MetaSlotPanel : MonoBehaviour
         return slot;
     }
 
+    private List<MetaItem> GetItems() 
+    {
+        List<MetaItem> currentItems = new List<MetaItem>();
+
+        foreach (var slot in _slots)
+        {
+            currentItems.Add(slot.CurrentItem);
+        }
+
+        return currentItems;
+    }
+
     private bool IsFull()
     {
         int countSlots = _slots.Count;
@@ -71,5 +97,14 @@ public class MetaSlotPanel : MonoBehaviour
         }
 
         return fullSlots == countSlots;
+    }
+
+    private void ClearSlots()
+    {
+        foreach (MetaSlot slot in _slots)
+        {
+            if (slot.IsFull())
+                slot.DeleteItem();
+        }
     }
 }
