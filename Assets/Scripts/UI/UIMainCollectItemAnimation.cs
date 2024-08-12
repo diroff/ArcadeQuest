62707d366ac77065cddec8174333eca7defd9a8f
Диэр <danil.aprelskiy@mail.ru>
2024-08-12 16:18:20 +0300

    fix: ui main collect item animation particle disable

--- Assets/Scripts/UI/UIMainCollectItemAnimation.cs
+++ Assets/Scripts/UI/UIMainCollectItemAnimation.cs
@@ -9,12 +9,10 @@ public class UIMainCollectItemAnimation : UICollectItemAnimation
     protected override IEnumerator CollectAnimation(Item item)
     {
         var itemAnimation = item.GetComponent<AnimationScript>();
-        var itemParticle = item.GetComponentInChildren<ParticleSystem>();
         var meshRenderer = item.GetComponent<MeshRenderer>();
 
         meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
         Destroy(itemAnimation);
-        Destroy(itemParticle.gameObject);
 
         float startTime = Time.time;
 
